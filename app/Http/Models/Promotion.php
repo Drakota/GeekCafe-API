df50c449080afc659892510919d136c97928eca2
Jonathan Bouchard <jonathaneutron@hotmail.com>
2017-10-03 13:20:09 -0400

    Promotions Seeder

---
+++ app/Http/Models/Promotion.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Promotion extends Model
+{
+    protected $table = 'promotions';
+    protected $primaryKey = 'id';
+}
