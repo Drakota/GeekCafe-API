24abef1d3fc4f47ccd6d0d495c9b46c2c81c2902
Jonathan Bouchard <jonathaneutron@hotmail.com>
2017-11-29 02:25:20 -0500

    Added overlayed images

--- app/Http/Transformers/ItemSubitemTransformer.php
+++ app/Http/Transformers/ItemSubitemTransformer.php
@@ -18,6 +18,7 @@ class ItemSubitemTransformer extends TransformerAbstract
           'price' => $itemsubitem->subitem->price,
           'is_topping' => (boolean)$itemsubitem->subitem->is_topping,
           'image' => env('IMG_URL') . $itemsubitem->subitem->image_id,
+          'big_image' => isset($itemsubitem->subitem->big_image_id) ? env('IMG_URL') . $itemsubitem->subitem->big_image_id : null,
         ];
     }
 }
